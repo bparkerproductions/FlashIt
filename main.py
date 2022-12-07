33c6e3dceece2a271bba0da962cee0d0887b2b40
Brandon Parker <>
2022-12-07 23:16:19 +0000

    Create userchoice skeleton structure

--- main.py
+++ main.py
@@ -3,7 +3,32 @@ class CardSet:
   def __init__(self):
     # Create empty dictionary, this will be used to keep track of added cards
     cardSet = {}
-    self.printInstructions()
+
+    while True:
+      self.printInstructions()
+      userChoice = input('What would you like to do?\n')
+
+      if userChoice == 'exit':
+        print('Good bye')
+        break
+
+      elif userChoice == 'add':
+        self.addCard()
+        
+      elif userChoice == 'delete':
+        if len(cardSet) == 0:
+          print('There are no cards to delete')
+        else:
+          self.deleteCard()
+
+      elif userChoice == 'update':
+        if len(cardSet) == 0:
+          print('There are no cards to update')
+        else:
+          self.updateCard()
+          
+      else:
+        print('This command is not recognized, please try again')
 
   def printInstructions(self):
     print('''
@@ -11,6 +36,16 @@ class CardSet:
     To add: use "add"
     To delete: use "delete"
     To update use "update"
+    To exit, use "exit"
     ''')
+
+  def deleteCard(self):
+    print('Choose a card to delete')
+
+  def updateCard(self):
+    print('Choose a card to update')
+
+  def addCard(self):
+    print('Choose a card to add')
 
 biology = CardSet()
