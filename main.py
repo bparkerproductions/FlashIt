61a945b1d5f37eacedcfbbd43f0752f652be7b7a
Brandon Parker <>
2022-12-07 23:02:43 +0000

    Add printed instructions

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ class CardSet:
   def __init__(self):
     # Create empty dictionary, this will be used to keep track of added cards
     cardSet = {}
+    self.printInstructions()
 
+  def printInstructions(self):
+    print('''
+    Welcome to Flash It! 
+    To add: use "add"
+    To delete: use "delete"
+    To update use "update"
+    ''')
 
 biology = CardSet()
