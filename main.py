8c1227349b0ffda854abc0c264cdd782588299a6
Brandon Parker <>
2022-12-07 22:55:38 +0000

    Add Cardset class

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import statistics
+class CardSet:
+  # A card set is a deck of study cards that will contain multiple cards. It can be reviewed and studied
+  def __init__(self):
+    # Create empty dictionary, this will be used to keep track of added cards
+    cardSet = {}
 
-help(statistics)
+
+biology = CardSet()
